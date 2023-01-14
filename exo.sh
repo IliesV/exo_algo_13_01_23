a9dda16774170c500faa447b428380115fe8b6b4
iliesV <ilies1512.viellard@yahoo.fr>
2023-01-14 14:47:42 +0100

    Troisième exo

--- exo.sh
+++ exo.sh
@@ -6,21 +6,35 @@ read -p "Veuillez entrer un nombre : " input
 	do
 		echo $i;
 	done
-
-
 }
-
 
 function exo2(){
 read -p "Veuillez entrer du texte  : " input
 
 nbvoyelle=$(echo $input | grep -o -i "[aeiouy]" | wc --lines)
 echo $nbvoyelle;
-
-
-
 }
 
 
+function exo3(){
+read -p "Combien d'entiers dans le tableau ? " nbEntiers
+	for ((i=1 ; i<=nbEntiers ; i++ ));
+	do
+		read -p "Entrez l'entier numéro "$i" : " entier
+		entierArray+=($entier)
+	done
 
-exo2;
+echo "Le plus grand élément parmi les nombres donnés est : $(plusGrand "${entierArray[@]}")"
+
+}
+
+function plusGrand(){
+max=${1}
+	for i in "${@:2}";
+	do
+		((i > max)) && max=$i
+	done
+echo $max
+}
+
+exo3;
